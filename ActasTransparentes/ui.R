07aba13abef48d83176b4c31610ab5fdf5cf61d4
frans <FvD@users.noreply.github.com>
2016-10-04 18:44:35 -0600

    solve merge conflict

--- ActasTransparentes/ui.R
+++ ActasTransparentes/ui.R
@@ -13,16 +13,46 @@ library(shiny)
 library(shinydashboard)
 
 sidebar <- dashboardSidebar(
-      uiOutput("acta_sel"),
-      sliderInput(inputId = "no_palabras", "Numero Palabras", min=30, max=500, value=100)
+  sidebarMenu(
+    menuItem("Tablero central", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Nube Palabras", tabName = "nube_palabras", icon=icon("cloud"),
+            menuSubItem("Genera nube", tabName = "palabras"),
+              uiOutput("acta_sel"),
+             sliderInput(inputId = "no_palabras", 
+                        "Numero Palabras", 
+                        min=30, max=500, value=100)),
+    menuItem("Busquedas", tabName = "busquedas", icon = icon("search"))
+  )
 )
 
 body <- dashboardBody(    
-       plotOutput("wordcloud")
+  tabItems(
+    tabItem(tabName = "dashboard",
+            h2("Tablero de Actas Transparentes"),
+            fluidRow(
+              # A static valueBox
+              valueBox(90, "Actas procesadas", icon = icon("file-pdf-o"), color = "green"),
+              
+              # Dynamic valueBoxes
+              valueBox(2, "Actas Nuevas", icon = icon("files-o"), color = "blue"),
+              
+              valueBox(3, "Numero de usuarios", icon = icon("users"), color = "orange")
+              
+            )
+    ),
+    
+    tabItem(tabName = "palabras",
+            h2("Nube de Palabras"),
+            plotOutput("wordcloud")
+    ),
+  
+   tabItem(tabName = "busquedas",
+          h2("Busquedas en Actas")
+   )
+  )
 )
 
-
-dashboardPage(
+dashboardPage(skin="green",
   dashboardHeader(title = "ActasTransparentes"),
   sidebar,
   body
